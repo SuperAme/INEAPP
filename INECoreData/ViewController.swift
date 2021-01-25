3c6f72bbb6ea8e9a7de86f9086de82732af45a56
Américo <americo.meneses.contractor@bbva.com>
2021-01-25 15:56:26 -0600

    Initial Commit

---
+++ INECoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  INECoreData
+//
+//  Created by IDS Comercial on 1/25/21.
+//  Copyright © 2021 Américo MQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
